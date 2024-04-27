f999be70de32513238776a00060fb07d24d372ad
akshatkumar <akshatk806@gmail.com>
2024-04-27 14:33:25 +0530

    added the Role enforcement using seeding

---
+++ Constants/RoleConstant.cs
@@ -0,0 +1,11 @@
+namespace Product_Management.Constants
+{
+    public enum Roles
+    {
+        Admin,
+        User
+    }
+    public class RoleConstant
+    {
+    }
+}
---
+++ Data/AuthDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Product_Management.Models.DomainModels;
+
+namespace Product_Management.Data
+{
+    public class AuthDbContext : IdentityDbContext<UserModel>
+    {
+        public AuthDbContext(DbContextOptions options) : base (options)
+        {
+
+        }
+    }
+}
